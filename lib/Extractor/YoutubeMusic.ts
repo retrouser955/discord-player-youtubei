28cbb6350408e0715acf4c777d9de364a1a0e43a
Retro <70205403+retrouser955@users.noreply.github.com>
2024-09-24 19:31:11 +0630

    feat(metadata): Finished playlist metadata querying

--- lib/Extractor/YoutubeMusic.ts
+++ lib/Extractor/YoutubeMusic.ts
@@ -1,7 +1,8 @@
-import { BaseExtractor, ExtractorInfo, ExtractorSearchContext, QueryType, SearchQueryType, Track, Util } from "discord-player";
+import { BaseExtractor, ExtractorInfo, ExtractorSearchContext, Playlist, QueryType, SearchQueryType, Track, Util } from "discord-player";
 import type Innertube from "youtubei.js";
 import { YoutubeiExtractor } from "./Youtube";
 import { extractSearchQueryType, isYoutubeMusic } from "../common/isYoutubeMusic";
+import { YTNodes } from "youtubei.js";
 
 export type YoutubeMusicExtractorOptions = {
     youtubeiExtractor: YoutubeiExtractor
@@ -50,6 +51,74 @@ export class YoutubeMusicExtractor extends BaseExtractor<YoutubeMusicExtractorOp
                     tracks: [trackConstruct]
                 }
             }
+            case "playlist": {
+                const plURL = new URL(query)
+                const plID = plURL.searchParams.get("list")
+                if(!plID) throw new Error("That is not a valid youtube playlist")
+                let playlistInfo = await this.innertube.music.getPlaylist(plID)
+
+                if(!playlistInfo.header?.is(YTNodes.MusicResponsiveHeader)) throw new Error("Error: MusicResponsiveHeader not found (no metadata queries)")
+                
+                const playlistMeta = playlistInfo.header.as(YTNodes.MusicResponsiveHeader)
+
+                const playlist = new Playlist(this.context.player, {
+                    title: playlistMeta.title.text || "UNKNOWN PLAYLIST",
+                    thumbnail: playlistMeta.thumbnail?.contents.at(0)?.url || "https://upload.wikimedia.org/wikipedia/commons/d/d8/YouTubeMusic_Logo.png",
+                    tracks: [],
+                    type: "playlist",
+                    author: {
+                        name: playlistMeta.strapline_text_one.text!,
+                        url: "retrouser955://discord-player-youtubei/no-playlist-author-url"
+                    },
+                    id: plID,
+                    url: query,
+                    source: "youtube",
+                    description: playlistMeta.description?.description.text!
+                })
+
+                const tracks = this.#mapPlaylistTracks(playlistInfo, context, playlist)
+
+                while(playlistInfo.has_continuation) {
+                    playlistInfo = await playlistInfo.getContinuation();
+
+                    tracks.push(...this.#mapPlaylistTracks(playlistInfo, context, playlist))
+                }
+
+                playlist.tracks = tracks
+
+                return {
+                    playlist,
+                    tracks
+                }
+            }
         }
+    }
+
+    #mapPlaylistTracks(playlistInfo: Awaited<ReturnType<Innertube['music']['getPlaylist']>>, context: ExtractorSearchContext, playlist: Playlist): Track[] {
+        if(!playlistInfo.contents) return []
+        const tracks = playlistInfo.contents?.filterType(YTNodes.MusicResponsiveListItem).map(v => {
+            const duration = Util.buildTimeCode(Util.parseMS((v.duration?.seconds || 0) * 1000))
+            const raw = {
+                duration_ms: (v.duration?.seconds || 0) * 1000,
+                live: false,
+                duration
+            }
+
+            return new Track(this.context.player, {
+                title: v.title!,
+                duration,
+                author: v.artists?.map((v) => v.name).join(", "),
+                requestedBy: context.requestedBy,
+                url: `https://music.youtube.com/watch?v=${v.id}`,
+                raw,
+                playlist,
+                source: "youtube",
+                requestMetadata: async () => raw,
+                metadata: raw,
+                live: false
+            })
+        })
+
+        return tracks
     }
 }
