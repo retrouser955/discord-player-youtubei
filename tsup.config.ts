476821141905e1aeb2869f83a46447a7df112934
root <root@LAPTOP-IRE4CR39.localdomain>
2025-01-20 15:12:45 +0800

    fix: types

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-    format: "cjs",
+    format: ["cjs", "esm"],
     entry: ['./lib/index.ts', "./lib/experimental/index.ts"],
     outDir: "./dist",
     skipNodeModulesBundle: true,
